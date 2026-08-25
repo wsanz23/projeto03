76505a196e4eb26bddb33a1543e08bd9650d2c02
lucas gabriel <lucas.silva654@aluno.cps.sp.gov.br>
2026-08-25 20:24:51 -0300

    atualizacao de dados

---
+++ curriculo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Currículo Lucas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Lucas Gabriel dos Santos Silva</h1>
+        <p><strong>E-mail</strong>lucasgabrielc642@gmail.com</p>
+        <p><strong>Telefone</strong>(11) 939344-661</p>
+    </header>
+    <main>
+        <section>
+            <h2>Objetivo</h2>
+            <p>Busco oportunidade no estágio na área de T.I. Atualmente curso Desenvolvimento de Sistemas e busco constante desenvolvimento profissional.</p>
+        </section>
+
+        <section>
+            <h2>Formação</h2>
+            <strong>PEI. E.E Orígenes Lessa </strong>
+             <p>Ensino médio completo</p>
+            <strong>Etec Juscelino Kubitschek de Oliveira</strong>
+             <p>Curso técnico(cursando): Desenvolvimento de sistemas</p>
+        </section>
+
+        <section>
+            <h2>Experiencia</h2>
+            <p>Mart serviços e terceirizações LTDA</p>
+            <strong>Cargo:</strong><p>Auxiliar de produção</p>
+            <strong>Período</strong><p>12/05/25 há 25/08/26</p>
+        </section>
+    </main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    curriculos
+    <header>
+        <h1>Meu currículo</h1>
+        <p>Bem vindo á minha pagina principal</p>
+    </header>
+    <a href="curriculo.php" class="botao">
+Ver currículo
+</a>
+
 </body>
 </html>
