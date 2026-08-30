e83be1ab93293423adc893150036cabe86b60b58
Wendell <wendellsantosxn@gmail.com>
2026-08-30 00:08:52 -0300

    curriculo

---
+++ curriculo-wendell.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Currículo Wendell</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Wendell Santos Souza</h1>
+        <p><strong>E-mail</strong>wendellsantosxn@gmail.com</p>
+        <p><strong>Telefone</strong>(11) 958901344</p>
+    </header>
+    <main>
+        <section>
+            <h2>Objetivo</h2>
+            <p>Busco oportunidade no estágio na área de ti Atualmente curso Desenvolvimento de Sistemas e busco constante desenvolvimento profissional.</p>
+        </section>
+
+        <section>
+            <h2>Formação</h2>
+            <strong>E.E José Fernando Abbud</strong>
+             <p>Ensino médio completo</p>
+            <strong>Etec Juscelino Kubitschek de Oliveira</strong>
+             <p>Curso técnico(cursando): Desenvolvimento de sistemas</p>
+        </section>
+
+        <section>
+            <h2>Experiencia</h2>
+            
+            <strong>Cargo:</strong><p>Operador de produção</p>
+            <strong>Período</strong><p>18/06/25 \ atual</p>
+        </section>
+    </main>
+</body>
+</html>
